01255190697a3ab25bc23ddcf1d21b6335241cb6
Jeff D. Hamann <jeff.hamann@forestinformatics.com>
2004-02-04 00:00:00 +0000

    version 0.7-1

--- R/systemfit.R
+++ R/systemfit.R
@@ -1,6 +1,26 @@
-### this is the systemfit project....
+###	$Id: systemfit.R,v 1.19 2004/02/04 02:34:01 hamannj Exp $	
+###
+###            Simultaneous Equation Estimation for R
+###
+### Copyright 2002-2004 Jeff D. Hamann <jeff.hamann@forestinformatics.com>
+###                     Arne Henningsen <http://www.arne-henningsen.de>
+###
+### This file is part of the nlsystemfit library for R and related languages.
+### It is made available under the terms of the GNU General Public
+### License, version 2, or at your option, any later version,
+### incorporated herein by reference.
+###
+### This program is distributed in the hope that it will be
+### useful, but WITHOUT ANY WARRANTY; without even the implied
+### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+### PURPOSE.  See the GNU General Public License for more
+### details.
+###
+### You should have received a copy of the GNU General Public
+### License along with this program; if not, write to the Free
+### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+### MA 02111-1307, USA
 
-#	$Id: systemfit.R,v 1.18 2003/11/10 20:19:04 hamannj Exp $	
 
 systemfit <- function( method,
                         eqns,
